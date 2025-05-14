3503f0f41b5b2ee9b48ae48dec78174e053a9381
HossamAhmadMohamed <ugs.141252@ci.suez.edu.eg>
2025-05-14 22:20:13 +0300

    Provider Example

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,14 @@ import 'main_provider.dart' as provider;
 import 'main_riverpod.dart' as riverpod;
 
 void main() {
+
+
+    /* Very Important for Success login
+  {
+    "email": "hossaaamahmed6@gmail.com",
+    "password": "hossam1234567"
+}
+  */
   const currentFlavor = AppFlavor.provider;
 
   switch (currentFlavor) {
